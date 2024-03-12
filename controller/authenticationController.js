abf64d5cd8e06d7ffde2b5190e6a4f755fc8fe77
kumar@kashish <mobiledevelopers74@gmail.com>
2024-03-12 15:54:02 +0530

    change

--- controller/authenticationController.js
+++ controller/authenticationController.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import user from "../models/User.js";
+import user from "../models/user.js";
 import identificationSchema from "../models/identificationSchema.js";
 import familyMember from "../models/familyMember.js";
 import addressSchema from "../models/addressSchema.js";

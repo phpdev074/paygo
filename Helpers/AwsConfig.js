e4cdce67ff953ed344fee4178b396c8d13156f34
kumar@kashish <mobiledevelopers74@gmail.com>
2024-03-13 17:44:14 +0530

    change

--- Helpers/AwsConfig.js
+++ Helpers/AwsConfig.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import { S3Client } from '@aws-sdk/client-s3';
 dotenv.config();
 const accessKey = process.env.ACCESS_KEY_S3_BUCKET;
-const secretAccessKey = process.env.S_ACCESS_KEY_S3_BUKCET;
+const secretAccessKey = process.env.SECRET_ACCESS_KEY_S3_BUKCET;
 const region = process.env.REGION;
 export const s3Client = new S3Client({
   region: region,

46c275b4361d09d2505ae5f31237b258a30d96bf
phpdev047 <phpdev047@gmail.com>
2024-03-20 16:24:37 +0530

    change

--- controller/userController.js
+++ controller/userController.js
@@ -58,16 +58,89 @@ export const getUserProfile = async(req,res)=>{
 export const editUserProfile = async(req,res)=>{
     try {
             const userId = req.user
-            const userOId= new mongoose.Types.ObjectId(userId)
-            const updates = req.body;
-            const user = await User.findByIdAndUpdate({_idu:userOId}, updates, { new: true });
-            if(getUserProfile) 
+            const userOId = new mongoose.Types.ObjectId(userId)
+            const {
+                name,
+                email,
+                image,
+                phoneNumber,
+                countryCode,
+                gender,
+                dateOfBirth,
+                birthPlace,
+                nationality,
+                educationStatus,
+                password,
+                country,
+                currentAddress,
+                region,
+                zone,
+                city,
+                subCity,
+                woreda,
+                houseNumber,
+                landlineNumber,
+                facialPicture,
+                identificationCard,
+                fName,
+                mName,
+                gFName,
+                gMName,
+                maritalStatus,
+                fullNameOfSpouse,
+                totalFamilyNumber,
+                totalMaleNumber,
+                totalFemaleNumber,
+                typeOfIdentificationCard,
+                idNumber,
+                occupationalStatus,
+                incomeNature
+              } = req.body;
+            const userData = await user.findById({_id:userOId});
+            if (name) userData.name = name;
+            if (email) userData.email = email;
+            if (image) userData.image = image;
+            if (phoneNumber) userData.phoneNumber = phoneNumber;
+            if (countryCode) userData.countryCode = countryCode;
+            if (gender) userData.gender = gender;
+            if (dateOfBirth) userData.dateOfBirth = dateOfBirth;
+            if (birthPlace) userData.birthPlace = birthPlace;
+            if (nationality) userData.nationality = nationality;
+            if (educationStatus) userData.educationStatus = educationStatus;
+            if (password) userData.password = password;
+            if (country) userData.country = country;
+            if (currentAddress) userData.currentAddress = currentAddress;
+            if (region) userData.region = region;
+            if (zone) userData.zone = zone;
+            if (city) userData.city = city;
+            if (subCity) userData.subCity = subCity;
+            if (woreda) userData.woreda = woreda;
+            if (houseNumber) userData.houseNumber = houseNumber;
+            if (landlineNumber) userData.landlineNumber = landlineNumber;
+            if (facialPicture) userData.facialPicture = facialPicture;
+            if (identificationCard) userData.identificationCard = identificationCard;
+            if (fName) userData.fName = fName;
+            if (mName) userData.mName = mName;
+            if (gFName) userData.gFName = gFName;
+            if (gMName) userData.gMName = gMName;
+            if (maritalStatus) userData.maritalStatus = maritalStatus;
+            if (fullNameOfSpouse) userData.fullNameOfSpouse = fullNameOfSpouse;
+            if (totalFamilyNumber) userData.totalFamilyNumber = totalFamilyNumber;
+            if (totalMaleNumber) userData.totalMaleNumber = totalMaleNumber;
+            if (totalFemaleNumber) userData.totalFemaleNumber = totalFemaleNumber;
+            if (typeOfIdentificationCard) userData.typeOfIdentificationCard = typeOfIdentificationCard;
+            if (idNumber) userData.idNumber = idNumber;
+            if (occupationalStatus) userData.occupationalStatus = occupationalStatus;
+            if (incomeNature) userData.incomeNature = incomeNature;
+            await userData.save();
+
+            if(getuserDataProfile) 
             {
-                handleSuccess(res,getUserProfile,"User profile fetched SuccessFully",statusCode?.OK)
+                handleSuccess(res, userData,"User profile updated SuccessFully",statusCode?.OK)
             }
             else
             {
-                handleError(res,"User Profile fetch Failed",statusCode?.BAD_REQUEST)
+                handleError(res,"User Profile update Failed",statusCode?.BAD_REQUEST)
             }
     } catch (error) {
         handleError(res,error.message,statusCode?.INTERNAL_SERVER_ERROR)
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getUserDetails,userUpdateUserStatus,getUserProfile } from '../controller/userController.js';
+import { getUserDetails,userUpdateUserStatus,getUserProfile,editUserProfile } from '../controller/userController.js';
 import verifyAuthToken from "../middileware/JwtVerify.js";
 const userRouter = express.Router();
 userRouter.get("/list-user-details",getUserDetails)
 userRouter.put("/update-user-status",userUpdateUserStatus)
 userRouter.get("/get-user-profile",verifyAuthToken,getUserProfile)
+userRouter.put("/edit-user-profile",verifyAuthToken,editUserProfile)
 export default userRouter;

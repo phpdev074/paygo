01330351191ff21f94c46d58b5cf6aa2ab20ad53
phpdev047 <phpdev047@gmail.com>
2024-03-26 17:55:07 +0530

    chnage

--- controller/insuranceController.js
+++ controller/insuranceController.js
@@ -10,7 +10,8 @@ export const createInsuranceLoan = async(req,res)=>{
             const {vehicleName,vehicleBrand,vehicleModel,color,licensePlateNo,vehicleYear,ownerId,agentPhoneNumber,agentCode,agentApp,fuelType,vehicleValue,carCondition,loanTerm,vehicleRegistrationNo,registrationDate,recordedMilage,bankName,bankAccountNumber,insuranceCompany,drivingLicenceImage,formFilledBy,employmentAddress,applicationDate,vehicleLibre,libreFrontPic,libreBackPic,engineNo,vehiclePhoto} = req.body
             const createInsuranceLoans = await InsuranceLoan.create({userId:userOId,vehicleName,vehicleBrand,vehicleModel,color,agentPhoneNumber,agentCode,agentApp,licensePlateNo,vehicleYear,ownerId,fuelType,vehicleValue,carCondition,loanTerm,vehicleRegistrationNo,registrationDate,recordedMilage,bankName,bankAccountNumber,insuranceCompany,drivingLicenceImage,formFilledBy,employmentAddress,applicationDate,vehicleLibre,libreFrontPic,libreBackPic,engineNo,vehiclePhoto})
             if(createInsuranceLoans)
-            {
+            {   
+                //hanling the data 
                 handleSuccess(res,createInsuranceLoans,'You veichle Insurance created successfully',statusCode?.OK)
             }
             else
